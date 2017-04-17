7f753149fa46bf4812e6088625929a065f549d1a
caoshengxiang <1104729815@qq.com>
2017-04-17 17:44:35 +0800

    fetch测试>>>server端图片上传以及阅览,借口测试

--- client/404.vue
+++ client/404.vue
@@ -1,35 +1,53 @@
-
 <template>
-	<div id="content"> 
-		<div class="scale"><a href="/">BS 404</a></div>
-	</div>
+    <div id="content">
+        <div class="scale"><a href="/">BS 404</a></div>
+    </div>
 </template>
+<script>
+    import fetch from './utils/fetch'
+    export default {
+        data() {
+            return {
+                d: {}
+            }
+        },
+        created() {
+//            fetch.post('http://localhost:3000/people', {
+//                name: "yan",
+//                sex: "man",
+//                age: "18",
+//            })
+        }
+    }
+</script>
 <style>
-	.scale {
-		background: red;
-		width: 100px;
-		height: 100px;
-		transition: all 1s;
-		animation: scale 1s ease;
-		animation-fill-mode: forwards;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		font-size: 30px;
-		margin: 100px auto;
-	}
-	.scale:hover {
+    .scale {
+        background: red;
+        width: 100px;
+        height: 100px;
+        transition: all 1s;
+        animation: scale 1s ease;
+        animation-fill-mode: forwards;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 30px;
+        margin: 100px auto;
+    }
 
-	}
-	@keyframes scale {
-		30%{
-			transform: rotate(90deg);
-		}
-		63%{
-			transform: rotate(-90deg);
-		}
-		100% {
-			transform: scale(1.2);
-		}
-	}
+    .scale:hover {
+
+    }
+
+    @keyframes scale {
+        30% {
+            transform: rotate(90deg);
+        }
+        63% {
+            transform: rotate(-90deg);
+        }
+        100% {
+            transform: scale(1.2);
+        }
+    }
 </style>
